7150fc5690701a0bfd149847b48e587c8614d95f
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2025-05-28 15:38:00 +0900

    refactor: Lombok적용

--- src/main/java/com/jnu/capstone/dto/ApplicantResponseDto.java
+++ src/main/java/com/jnu/capstone/dto/ApplicantResponseDto.java
@@ -1,7 +1,12 @@
 package com.jnu.capstone.dto;
 
 import com.jnu.capstone.entity.Applicant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+// 신청자 목록 조회 DTO
+@Getter
+@AllArgsConstructor
 // 신청자 목록 조회 DTO
 public class ApplicantResponseDto {
     private int applicantId;
@@ -9,35 +14,6 @@ public class ApplicantResponseDto {
     private String department;
     private String applicationText;
     private boolean isAccepted;
-
-    public ApplicantResponseDto(int applicantId, String nickname, String department, String applicationText, boolean isAccepted) {
-        this.applicantId = applicantId;
-        this.nickname = nickname;
-        this.department = department;
-        this.applicationText = applicationText;
-        this.isAccepted = isAccepted;
-    }
-
-    public int getApplicantId() {
-        return applicantId;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public String getApplicationText() {
-        return applicationText;
-    }
-
-    public boolean isAccepted() {
-        return isAccepted;
-    }
-
     // 엔티티에서 DTO로 변환하는 편의 메서드
     public static ApplicantResponseDto fromEntity(Applicant applicant) {
         return new ApplicantResponseDto(
--- src/main/java/com/jnu/capstone/dto/GatheringDetailResponseDto.java
+++ src/main/java/com/jnu/capstone/dto/GatheringDetailResponseDto.java
@@ -1,5 +1,13 @@
 package com.jnu.capstone.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GatheringDetailResponseDto {
     private int postId;
     private String title;
@@ -10,48 +18,4 @@ public class GatheringDetailResponseDto {
     private String time;
     private boolean isClosed;
     private int authorId;
-
-    public GatheringDetailResponseDto(int postId, String title, String dDay, String contents, String place, String gender, String time, boolean isClosed, int authorId) {
-        this.postId = postId;
-        this.title = title;
-        this.dDay = dDay;
-        this.contents = contents;
-        this.place = place;
-        this.gender = gender;
-        this.time = time;
-        this.isClosed = isClosed;
-        this.authorId = authorId;
-    }
-
-    // Getters and Setters
-    public int getPostId() { return postId; }
-    public void setPostId(int postId) { this.postId = postId; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getDDay() { return dDay; }
-    public void setDDay(String dDay) { this.dDay = dDay; }
-
-    public String getContents() { return contents; }
-    public void setContents(String contents) { this.contents = contents; }
-
-    public String getPlace() { return place; }
-    public void setPlace(String place) { this.place = place; }
-
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
-
-    public String getTime() { return time; }
-    public void setTime(String time) { this.time = time; }
-
-    public boolean isClosed() { return isClosed; }
-    public void setClosed(boolean closed) { isClosed = closed; }
-    public int getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(int authorId) {
-        this.authorId = authorId;
-    }
 }
