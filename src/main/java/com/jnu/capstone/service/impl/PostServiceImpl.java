1bc4293e016a8d10925e56ac783819864472c0ba
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2025-06-07 00:56:03 +0900

    feat: 검색 기능 구현

--- src/main/java/com/jnu/capstone/service/impl/PostServiceImpl.java
+++ src/main/java/com/jnu/capstone/service/impl/PostServiceImpl.java
@@ -3,9 +3,13 @@ package com.jnu.capstone.service.impl;
 import com.jnu.capstone.dto.MyPostSimpleDto;
 import com.jnu.capstone.entity.Post;
 import com.jnu.capstone.repository.PostRepository;
+import com.jnu.capstone.repository.UserRepository;
 import com.jnu.capstone.service.PostService;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import com.jnu.capstone.entity.BoardType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<Post> getAllPosts() {
@@ -60,4 +65,14 @@ public class PostServiceImpl implements PostService {
                 ))
                 .collect(Collectors.groupingBy(MyPostSimpleDto::getBoardType));
     }
+    // ✅ 게시글 검색 기능 추가
+    @Override
+    public Page<Post> searchPostsByCampusAndKeyword(int userId, String keyword, List<BoardType> boardTypes, Pageable pageable) {
+        int campusId = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."))
+                .getCampus().getCampusId();
+
+        return postRepository.findByCampusAndKeywordAndBoardType(campusId, keyword, boardTypes, pageable);
+    }
+
 }
