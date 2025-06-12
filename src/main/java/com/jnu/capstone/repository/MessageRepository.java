7182f2e5a897e13ce7b8fcc8bc6d1398d26b3641
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2025-06-12 13:12:27 +0900

    fix: 채팅방 이전 대화 조회 기능 수정

--- src/main/java/com/jnu/capstone/repository/MessageRepository.java
+++ src/main/java/com/jnu/capstone/repository/MessageRepository.java
@@ -5,13 +5,17 @@ import com.jnu.capstone.entity.Chatroom;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    Page<Message> findByChatroomOrderBySendTimeAsc(Chatroom chatroom, Pageable pageable);
+    @Query("SELECT m FROM Message m JOIN FETCH m.sender WHERE m.chatroom = :chatroom ORDER BY m.sendTime ASC")
+    Page<Message> findByChatroomWithSender(@Param("chatroom") Chatroom chatroom, Pageable pageable);
+
     Optional<Message> findTopByChatroomOrderBySendTimeDesc(Chatroom chatroom);
 }
 
