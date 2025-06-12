b35c02eec1b80c611cf7859b89575169f1d8ae01
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2025-06-12 23:52:44 +0900

    fix: 웹소켓 수정

--- src/main/java/com/jnu/capstone/config/WebSocketConfig.java
+++ src/main/java/com/jnu/capstone/config/WebSocketConfig.java
@@ -3,12 +3,18 @@ package com.jnu.capstone.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 
 import java.util.Map;
 @Configuration
@@ -39,5 +45,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         // 메시지 보낼 때 prefix: /app
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (accessor != null) {
+                    System.out.println("📦 STOMP 프레임: " + accessor.getCommand());
+                    System.out.println("📬 헤더: " + accessor.toNativeHeaderMap());
+                }
+                return message;
+            }
+        });
     }
 }
