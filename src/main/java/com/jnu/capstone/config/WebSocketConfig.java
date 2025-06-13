369f88402d1c80b0a702bc150cccb6350a51f343
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2025-06-13 13:17:11 +0900

    fix: 웹소켓 수정

--- src/main/java/com/jnu/capstone/config/WebSocketConfig.java
+++ src/main/java/com/jnu/capstone/config/WebSocketConfig.java
@@ -1,66 +1,22 @@
 package com.jnu.capstone.config;
 
+import com.jnu.capstone.websocket.CustomWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.config.annotation.*;
 
-import java.util.Map;
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS()
-                .setInterceptors(new HttpSessionHandshakeInterceptor() {
-                    @Override
-                    public boolean beforeHandshake(
-                            ServerHttpRequest request,
-                            ServerHttpResponse response,
-                            WebSocketHandler wsHandler,
-                            Map<String, Object> attributes) throws Exception {
+    private final CustomWebSocketHandler customWebSocketHandler;
 
-                        System.out.println("🔍 [beforeHandshake] WebSocket 요청 도착: " + request.getURI());
-                        return super.beforeHandshake(request, response, wsHandler, attributes);
-                    }
-                });
+    public WebSocketConfig(CustomWebSocketHandler customWebSocketHandler) {
+        this.customWebSocketHandler = customWebSocketHandler;
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 클라이언트에서 구독하는 prefix: /topic
-        registry.enableSimpleBroker("/topic");
-        // 메시지 보낼 때 prefix: /app
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new ChannelInterceptor() {
-            @Override
-            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-                StompHeaderAccessor accessor =
-                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-                if (accessor != null) {
-                    System.out.println("📦 STOMP 프레임: " + accessor.getCommand());
-                    System.out.println("📬 헤더: " + accessor.toNativeHeaderMap());
-                }
-                return message;
-            }
-        });
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "/ws/chat")
+                .setAllowedOrigins("*");
     }
 }
